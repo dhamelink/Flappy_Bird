67adeef60fe55e9542a8382f96656a40e43bd8bf
dhamelink <90962088+dhamelink@users.noreply.github.com>
2023-11-24 11:26:58 +0100

    Flappy Bird 1.0

--- bird.js
+++ bird.js
@@ -1,11 +1,17 @@
+const dragonSprite = new Image();
+dragonSprite.src = "dragon.png";
+
 class Bird {
     constructor(){
         this.x = 150;
         this.y = 200;
         this.vy = 0;
-        this.width = 30;
-        this.height = 30;
+        this.originalWidth = 941;
+        this.originalHeight = 680;
+        this.width = this.originalWidth/20;
+        this.height = this.originalHeight/20;
         this.weight = 1;
+        this.frameX = 0;
 
     }
     update(){
@@ -26,12 +32,15 @@ class Bird {
     }
     draw(){
         ctx.fillStyle = "red";
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        //ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(dragonSprite, this.frameX * this.originalWidth, 0, this.originalWidth, this.originalHeight, this.x - 20, this.y - 12, this.width * 1.7, this.height * 1.7);
 
     }
     flap(){
         this.vy -= gamespeed;
-        console.log
+        if(this.frameX >= 3) this.frameX = 0;
+        else if (frame%5 === 0) this.frameX++;
+        console.log(this.frameX)
     }
 }
 const bird = new Bird();
--- main.js
+++ main.js
@@ -63,6 +63,7 @@ window.addEventListener("keydown", function(e){
 });
 window.addEventListener("keyup", function(e){
     if (e.code === "Space") spacePressed = false;
+    bird.frameX = 0;
 });
 
 const bang = new Image();
